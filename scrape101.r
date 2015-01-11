43bd3eeb0ad48ca58b5016c91b4d1c992a7d83ef
Hari Donthi <hari.donthi@gmail.com>
2015-01-11 11:54:52 -0500

    fixed comments

--- scrape101.r
+++ scrape101.r
@@ -1,12 +1,15 @@
-# Usage: Specify the list of companies to scrape on the companies
+# Output: Get some text following the substring "About" in a company's homepage
+# Input: Provide the list of companies to scrape in the line below
 
+companies <- c('hp','ebay','microsoft','cisco','google','facebook')
+
+
+# How does the program work?
 # 1. Get the html for a list of companies (the program prefixes http://www., and suffixes .com)
 # 2. Print the lengths of the html
 # 3. Find the text "About" in the html, and print the 100 characters following
 
-
 require(rvest)
-companies <- c('hp','ebay','microsoft','cisco','google','facebook')
 urls <- paste("http://www.", companies, ".com",sep="")
 homePages <- lapply(urls,html)
 homePagesHtml <- lapply(homePages,html_text)
